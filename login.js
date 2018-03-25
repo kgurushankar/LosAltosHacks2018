7c47e292948e0c038520e1fd5e8d2c40dad6b518
Unknown <keshavgurushankar@hotmail.com>
2018-03-25 03:51:43 -0700

    Auth Works!!

--- login.js
+++ login.js
@@ -3,14 +3,26 @@ module.exports = function (req, res) {
     var pass = req.body.password;
     const md5sum = require('crypto').createHash('md5').update(pass).digest('hex');
     var t = req.cookies.tournament;
+    var acc = {};
+    var db = require('./data.json');
     if (t == null) {
-        res.json('no tournament specified');
+        var keys = Object.keys(db);
+        for (var i = 0; i < keys.length; i++) {
+            var dat = db[keys[i]].accounts;
+            for (var j = 0; j < Object.keys(dat).length; j++) {
+                var key = Object.keys(dat)[j];
+                acc[key] = dat[key];
+            }
+        }
+    } else {
+        acc = db[t].accounts;
     }
-    var acc = require('./data/accounts.json')[t];
     if (acc[user] == md5sum) {
         var jwt = require('jsonwebtoken');
         const secret = require('fs').readFileSync('./index.html').toString();
-        var tkn = jwt.sign({ username: user }, secret);
+        var tkn = jwt.sign({ username: user, tournament: t }, secret);
         res.cookie('Authentication', tkn).json('success');
+    } else {
+        res.json("Invalid Credentials");
     }
 }
