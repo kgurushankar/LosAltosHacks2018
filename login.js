434c45d64b1c9663d60bc2f74ea281e9776f2c64
Unknown <keshavgurushankar@hotmail.com>
2018-03-25 04:19:52 -0700

    Oops

--- login.js
+++ login.js
@@ -5,18 +5,15 @@ module.exports = function (req, res) {
     var t = req.cookies.tournament;
     var acc = {};
     var db = require('./data.json');
-    if (t == null) {
-        var keys = Object.keys(db);
-        for (var i = 0; i < keys.length; i++) {
-            var dat = db[keys[i]].accounts;
-            for (var j = 0; j < Object.keys(dat).length; j++) {
-                var key = Object.keys(dat)[j];
-                acc[key] = { pass: dat[key], tournament: keys[i] };
-            }
+    var keys = Object.keys(db);
+    for (var i = 0; i < keys.length; i++) {
+        var dat = db[keys[i]].accounts;
+        for (var j = 0; j < Object.keys(dat).length; j++) {
+            var key = Object.keys(dat)[j];
+            acc[key] = { pass: dat[key], tournament: keys[i] };
         }
-    } else {
-        acc = db[t].accounts;
     }
+
     if (acc[user].pass == md5sum) {
         var jwt = require('jsonwebtoken');
         const secret = require('fs').readFileSync('./secret').toString();
